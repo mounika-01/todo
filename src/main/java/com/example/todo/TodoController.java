c82f7352f4c80cbfb7591a03d309eb978f42eded
Mounika <bsmounika15@gmail.com>
2024-03-31 11:38:02 +0000

    solution

---
+++ src/main/java/com/example/todo/TodoController.java
@@ -0,0 +1,55 @@
+package com.example.todo;
+
+import org.springframework.web.bind.annotation.";
+
+import java.util.*;
+
+import com.example.todo.TodoService;
+
+import com.example.todo.Todo;
+
+@RestController
+
+public class TodoController {
+
+TodoService todoService = new TodoService();
+
+@GetMapping("/todos")
+
+public ArrayList<Todo> getTodos() {
+
+return todoService.getTodos();
+
+}
+
+@GetMapping("/todos/{id}")
+
+public Todo getTodoById(@PathVariable("id") int id) {
+    return todoservice.getTodoById(id);
+}
+
+@PostMapping("/todos")
+
+public Todo addTodo(@RequestBody Todo todo) {
+
+return todoservice.addTodo(todo);
+
+}
+
+@PutMapping("/todos{id}")
+
+public Todo updateTodo(@PathVariable("id") int id, @RequestBody Todo todo) {
+
+return todoservice.updateTodo(id, todo);
+
+}
+
+@DeleteMapping("/todos/{id}")
+
+public void deleteTodo(@PathVariable int id) {
+
+todoService.deleteTodo(id);
+
+}
+
+}
